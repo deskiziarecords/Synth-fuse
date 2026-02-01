e719d5fa63f227217cc585aca608638c29098404
rjimenez <tijuanapaint@gmail.com>
2026-02-01 09:03:06 -0800

    folder moving

---
+++ lab/app.py
@@ -0,0 +1,14 @@
+if st.button(...):                    # 0 spaces
+    if st.session_state.cabinet...    # 4 spaces
+        with st.spinner(...):         # 8 spaces
+                                      # 12 spaces
+            async def cast_transmogrifai():  # 12 spaces
+                step1 = await...      # 16 spaces
+                step2 = await...      # 16 spaces
+                                      # 12 spaces
+            result = asyncio.run(...) # 12 spaces
+                                      # 8 spaces
+        st.balloons()                 # 8 spaces
+                                      # 4 spaces
+    else:                             # 4 spaces
+        st.error(...)                 # 8 spaces
