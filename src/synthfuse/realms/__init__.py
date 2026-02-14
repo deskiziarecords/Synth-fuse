4d2dad5ed828b02ee0b46569e5ea329ba6ee1af1
J Roberto Jimenez <tijuanapaint@gmail.com>
2026-02-13 20:58:57 -0800

    Create __init__.py

---
+++ src/synthfuse/realms/__init__.py
@@ -0,0 +1,7 @@
+from .factory import FactoryRealm
+from .playground import PlaygroundRealm
+from .automode import AutoModeRealm
+from .lab import LabRealm
+from .thermo import ThermoRealm
+
+__all__ = ['FactoryRealm', 'PlaygroundRealm', 'AutoModeRealm', 'LabRealm', 'ThermoRealm']
